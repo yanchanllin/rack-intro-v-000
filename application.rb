7eb4b1e24f9873d56622d9812697a20056c3b676
YanC Lin <yonalin1@gmail.com>
2018-03-08 00:05:12 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, Worlmy name is yan chan"
+    resp.write "Hello, my name is yan chan"
     resp.finish
   end
 
